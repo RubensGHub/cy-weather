82f6ce43b20ba5df3f3e7548886abd42b27b5faf
Rubenso <Rubens.tueux@cy-tech.fr>
2026-01-12 11:13:39 -0100

    ptit changement oklm

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from src.resources.weather_resource import router as weather_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 tags_metadata = [
@@ -33,6 +34,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+Instrumentator().instrument(app).expose(app)
 
 router = APIRouter(
     prefix="/api",
